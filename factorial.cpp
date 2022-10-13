88828c9655679b27d52dfa1a32abe43636b4656c
Andre Jarboe <andrejarboe@gmail.com>
2022-10-13 17:02:14 -0400

    refactor

--- factorial.cpp
+++ factorial.cpp
@@ -1,15 +1,30 @@
 // Write a function to calculate the factorial value of any integer as an argument.
 #include <iostream>
 
-int main (){
+int factorial(int a){
+    int fact = 1;
 
-    int factorial = 1;
-    int num = 6;
-
-
-    for(int i = num; i > 1; i--){
-        factorial *= i;
+    for(int i = a; i > 1; i--){
+        fact *= i;
     }
 
-    std::cout << "\n" << num << " factorial is: " << factorial;
+    return fact;
+}
+
+int main (){
+
+    // int factorial = 1;
+    int num;
+
+    std::cout << "Enter number to calculate the factorial of. ";
+    std::cin >> num;
+
+    // for(int i = num; i > 1; i--){
+    //     factorial *= i;
+    // }
+
+    
+
+    std::cout << "\n" << num << " factorial is: " << factorial(num);
+    std::cout << "\n";
 }
