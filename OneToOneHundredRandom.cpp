b1ce81b007cb395089f44bd512d1646725519c68
andrejarboe <andrejarboe@gmail.com>
2022-10-13 12:41:35 -0400

    first commit

---
+++ OneToOneHundredRandom.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	int n;
+	srand(time(0));
+	n = rand() % 91 + 10;
+	cout<<"The randomly selected number is :"<<n;
+    cout << "\n";
+	
+	return 0;
+}
